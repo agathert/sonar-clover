64e4a47a46e53cca565719606d421883a6f2bcd8
Tony Proum <t.proum@gmail.com>
2018-10-12 21:38:19 +0200

    fix(Build): fix build using simple travis pipeline

--- travis.sh
+++ travis.sh
@@ -5,13 +5,10 @@ set -euo pipefail
 function installTravisTools {
   mkdir -p ~/.local
   curl -sSL https://github.com/SonarSource/travis-utils/tarball/v50 | tar zx --strip-components 1 -C ~/.local
-  echo $(ls ~/.local/bin)
-  echo $(cat ~/.local/bin/install)
   source ~/.local/bin/install
 }
 
 installTravisTools
-
 source ~/.local/bin/installMaven35
 source ~/.local/bin/installJDK8
 
